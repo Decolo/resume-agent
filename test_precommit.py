2fabf6aaab454c864f5e23aca3eadc329c2472be
Decolo <caicheng.fe.dev@gmail.com>
2026-02-16 14:11:29 +0800

    chore: remove pre-commit test file

--- test_precommit.py
+++
@@ -1,9 +0,0 @@
-# Test file to verify pre-commit hooks
-
-
-def bad_formatting(x, y, z):
-    """This has bad formatting."""
-    return x + y + z  # No spaces around operators
-
-
-unused_var = "this will be caught"
