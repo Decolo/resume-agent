0c709a1960dc7855350b8921bf627d2d544a647c
Decolo <caicheng.fe.dev@gmail.com>
2026-02-16 14:11:22 +0800

    test: verify pre-commit hooks work

---
+++ test_precommit.py
@@ -0,0 +1,9 @@
+# Test file to verify pre-commit hooks
+
+
+def bad_formatting(x, y, z):
+    """This has bad formatting."""
+    return x + y + z  # No spaces around operators
+
+
+unused_var = "this will be caught"
